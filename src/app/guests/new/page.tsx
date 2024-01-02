0bf4c2f22a63154a145839a2b85075b9668d1ac9
Mohammed Aboukhatwa <ironhunter90@gmail.com>
2024-01-02 11:50:03 +0200

    Deleting unnecessary files

--- src/app/guests/new/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const newGuests = () => {
-  return <div>newGuests</div>;
-};
-
-export default newGuests;
--- src/app/new/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const newReservation = () => {
-  return <div>newReservation</div>;
-};
-
-export default newReservation;
